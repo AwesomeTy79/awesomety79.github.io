3f5aaf87afb4883d80ea9796e18dd9a8dfbfaf1a
AwesomeTy79 <32660892+AwesomeTy79@users.noreply.github.com>
2018-04-24 20:45:22 -0600

    Update js.js

--- code/js.js
+++ code/js.js
@@ -19,12 +19,12 @@ function filterFunction() {
     }
 }
 function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
+    if (!url) {url = window.location.href;}
+    var name = name.replace('/[\[\]]/g', "\\$&");
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
+    if (!results) {return null;}
+    if (!results[2]) {return '';}
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-(function () { var sayhi = getParameterByName('sayhi'); if (sayhi != null) { alert('hi'); } }()); 
+(function () { var sayhi = getParameterByName('sayhi'); if (sayhi != null) { alert('hi'); } }());
